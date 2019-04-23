95a150206df4f103dcf461a93f6fe86c8a718b30
gabrielsantos2019 <48133473+gabrielsantos2019@users.noreply.github.com>
2019-04-23 14:30:41 -0300

    Update ex4.php

--- ex4.php
+++ ex4.php
@@ -14,22 +14,4 @@
     else {
 
         print "\n é uma consoante \n";
-<?php
 
-
-    print "digite uma letra: \n ";
-
-    $a = fgets (STDIN);
-
-    if ($a == 'A' or $a == 'E' or $a == 'I' or $a == 'O' or $a == 'U') {
-
-        print "\n é uma vogal \n";
-
-    }
-
-    else {
-
-        print "\n é uma consoante \n";
-
-    }
-    }
