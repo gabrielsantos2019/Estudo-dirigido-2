a56ecb76dac53c26b16406f9434b7d63248bd401
gabrielsantos2019 <48133473+gabrielsantos2019@users.noreply.github.com>
2019-04-30 22:04:04 -0300

    Create ex16.php

---
+++ ex16.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+    print "Digite um número:\n";
+        $t1 = (float)fgets(STDIN);
+    
+    print "Digite outro número:\n";
+        $t2 = (float)fgets(STDIN);
+    
+    print "Digite outro número:\n";
+        $t3 = (float)fgets(STDIN);
+    
+    print "Digite outro número:\n";
+        $t4 = (float)fgets(STDIN);
+    
+    print "Digite outro número:\n";
+        $t5 = (float)fgets(STDIN);
+    
+        $soma = $t1+$t2+$t3+$t4+$t5;
+    
+        $media = $soma/5;
+    
+    print "A soma de todos os números é $soma, e a média deles é $media.\n\n";
